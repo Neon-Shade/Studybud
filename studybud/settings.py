c2975949ddf58301c5e456846ca8b60f27505441
Neom-Shade <Sobirjonovxushnud06@gmail.com>
2026-04-01 11:09:44 +0500

    Middleware dublikati ochirildi va whitenoise sozlandi

--- studybud/settings.py
+++ studybud/settings.py
@@ -48,8 +48,7 @@ AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # AYNAN SHU YERDA BO'LSIN!
-    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
